564b7bfbd3e42a587e90c4d606ff7be069a1be7d
ESTÉVEZ GIMÉNEZ DANIEL <deg061@inlumine.ual.es>
2023-11-27 14:25:55 +0100

    Clase Posicion

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -0,0 +1,29 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public class Posicion {
+    private int fila;
+    private char columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe estar entre 1 y 8, ambos inclusive.");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("La columna debe estar entre 'a' y 'h', ambas inclusive.");
+        }
+        this.columna = columna;
+    }
+}
+
