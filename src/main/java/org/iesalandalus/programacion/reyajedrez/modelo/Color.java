efee0cb4ba4982af99664169d0123576db470994
ESTÉVEZ GIMÉNEZ DANIEL <deg061@inlumine.ual.es>
2023-11-27 14:19:04 +0100

    Enumerado Color

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Color {
+    BLANCO("Blanco"),
+    NEGRO("Negro");
+
+    private String cadenaAMostrar;
+
+    private Color(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
